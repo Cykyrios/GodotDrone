2509a65342765afa99e812593de3c52108ec328b
Cykyrios <cykyrios@gmail.com>
2019-08-24 09:14:51 +0200

    Reduce yaw speed PID derivative
    Reduces yaw vibrations.

--- FlightController.gd
+++ FlightController.gd
@@ -55,7 +55,7 @@ func _ready():
 	pid_controllers[Controller.LATERAL_SPEED].set_coefficients(0.5, 0.05, 0.05)
 	pid_controllers[Controller.LATERAL_SPEED].set_clamp_limits(-0.5, 0.5)
 	
-	pid_controllers[Controller.YAW_SPEED].set_coefficients(3, 0, 0.1)
+	pid_controllers[Controller.YAW_SPEED].set_coefficients(3, 0, 0.05)
 	pid_controllers[Controller.YAW_SPEED].set_clamp_limits(-10, 10)
 	pid_controllers[Controller.ROLL_SPEED].set_coefficients(3, 0, 0.1)
 	pid_controllers[Controller.ROLL_SPEED].set_clamp_limits(-10, 10)
