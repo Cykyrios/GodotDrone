3eb639cfe6a25367198f570103ec1a72edcba5ef
Cykyrios <cykyrios@gmail.com>
2019-08-24 09:03:44 +0200

    Update FollowCamera behavior
    Adds a weighted position offset so the camera does not only trail behind.

--- FollowCamera.gd
+++ FollowCamera.gd
@@ -3,6 +3,8 @@ extends Camera
 export(NodePath) var target = null
 export(float, 0.1, 10.0) var target_distance = 2.0
 export(Vector3) var target_offset = Vector3.ZERO
+export(Vector3) var camera_offset = Vector3.ZERO
+export(float, 0, 1) var offset_weight = 0
 
 var target_node
 
@@ -14,7 +16,9 @@ func _ready():
 
 func _process(delta):
 	if target_node is Spatial:
-		var target_pos = target_node.global_transform.xform(target_offset)
+		var target_pos = target_node.global_transform.origin + target_offset
 		var pos_diff = target_pos - global_transform.origin
-		var offset = pos_diff.normalized() * target_distance
-		look_at_from_position(target_pos - offset, target_pos, Vector3.UP)
+		var offset = target_node.global_transform.xform(camera_offset)
+		var new_pos = target_pos - pos_diff.normalized() * target_distance
+		new_pos = new_pos.linear_interpolate(offset, offset_weight)
+		look_at_from_position(new_pos, target_pos, Vector3.UP)
