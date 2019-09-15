c0d147abadbebeaddad2aa68cd575f283e49950f
Cykyrios <cykyrios@gmail.com>
2019-09-15 08:45:38 +0200

    Add minimum rpm limit to propellers

--- Propeller.gd
+++ Propeller.gd
@@ -14,12 +14,14 @@ var torque = 0.0 setget set_torque, get_torque
 var rpm = 0.0 setget set_rpm, get_rpm
 var rpm_target = 0.0 setget set_rpm_target, get_rpm_target
 var max_rpm_change = 0.0
+var powered = true
 
 export (bool) var clockwise = false
 export (float, 0.0, 10000.0) var MAX_TORQUE = 1000.0
 export (float, 0.0, 30000.0) var MAX_RPM = 10000.0
 export (float, 0.0, 10.0) var LIFT_RATIO = 1.0
 export (float, 0, 50000) var RPM_ACCELERATION = 16000.0
+export (int, 0, 100) var MIN_POWER = 1
 
 
 func _ready():
@@ -30,13 +32,16 @@ func _ready():
 	max_rpm_change = MAX_TORQUE * RPM_ACCELERATION
 	
 	controller.set_coefficients(1000, 0, 1)
-	controller.set_clamp_limits(0, MAX_RPM)
+	controller.set_clamp_limits(MIN_POWER / 100.0 * MAX_RPM, MAX_RPM)
 
 
 func _physics_process(delta):
-	controller.set_target(thrust_target)
-	set_rpm_target(controller.get_output(get_thrust(), delta))
-	set_rpm(clamp(rpm_target, rpm - max_rpm_change * delta, rpm + max_rpm_change * delta))
+	if powered:
+		controller.set_target(thrust_target)
+		set_rpm_target(controller.get_output(get_thrust(), delta))
+		set_rpm(clamp(rpm_target, rpm - max_rpm_change * delta, rpm + max_rpm_change * delta))
+	else:
+		set_rpm(clamp(0, rpm - max_rpm_change * delta, rpm + max_rpm_change * delta))
 	set_torque(rpm / MAX_RPM / delta)
 	
 	var rot = rpm * PI / 30.0
