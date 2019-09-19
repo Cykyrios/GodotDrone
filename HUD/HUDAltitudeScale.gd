3fd67325a824aa987c9abeeee4ed4eab9221d293
Cykyrios <cykyrios@gmail.com>
2019-09-19 22:59:40 +0200

    Add HUD vertical speed and radio altimeter displays

--- HUD/HUDAltitudeScale.gd
+++ HUD/HUDAltitudeScale.gd
@@ -1,6 +1,8 @@
 extends Control
 
-onready var altitude_label = $HBoxContainer/Label
+onready var altitude_label = $HBoxContainer/VBoxContainer/LabelAltitude
+onready var radioaltitude_label = $HBoxContainer/VBoxContainer/LabelRadioAltitude
+onready var vertical_speed_label = $HBoxContainer/VBoxContainer/LabelVerticalSpeed
 onready var altitude_gauge = $HBoxContainer/AltitudeGauge
 
 var altitude = 0.0
@@ -19,7 +21,19 @@ func update_altitude(a : float, dt : float):
 		alt_sign = "-"
 	altitude_label.text = "%s%04d.%d" % [alt_sign, abs(a), (abs(a) - floor(abs(a))) * 10]
 	
-	var altitude_delta = 0.0
+	var vertical_speed = 0.0
 	if dt != 0:
-		altitude_delta = (altitude - altitude_prev) / dt
-	altitude_gauge.update_gauge(altitude_delta)
+		vertical_speed = (altitude - altitude_prev) / dt
+	altitude_gauge.update_gauge(vertical_speed)
+	var vs_sign = " "
+	if vertical_speed < 0:
+		vs_sign = "-"
+	vertical_speed_label.text = "%s%02d.%d" % [vs_sign, abs(vertical_speed),
+			(abs(vertical_speed) - floor(abs(vertical_speed))) * 10]
+
+
+func update_radio_altitude(a : float):
+	if a > 10 or a < 0:
+		radioaltitude_label.text = "R---"
+	else:
+		radioaltitude_label.text = "R%02d.%d" % [a, (a - floor(a)) * 10]
