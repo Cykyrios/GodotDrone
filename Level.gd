25d92aa8172b3a5cc6300625c3b48f0d51bc66f9
Cykyrios <cykyrios@gmail.com>
2019-10-06 10:15:40 +0200

    Fix reversed HUD power stick input.

--- Level.gd
+++ Level.gd
@@ -24,7 +24,7 @@ func _process(delta):
 	var velocity = fc.lin_vel
 	var altitude = fc.pos.y
 	var input = fc.input
-	var left_stick = Vector2(input[1], input[0])
+	var left_stick = Vector2(input[1], -input[0])
 	var right_stick = Vector2(input[2], input[3])
 	hud.update_hud(delta, angles.x, angles.z, angles.y, velocity, altitude, left_stick, right_stick)
 
