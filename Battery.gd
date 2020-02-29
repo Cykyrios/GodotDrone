d7ad0a9947893626c44f9c710058211eb1c59424
Cykyrios <cykyrios@gmail.com>
2020-02-29 23:18:38 +0100

    Add Battery class (not functional yet)

---
+++ Battery.gd
@@ -0,0 +1,30 @@
+extends Spatial
+
+class_name Battery
+
+
+const CELL_VOLTAGE_FULL = 4.2
+const CELL_VOLTAGE_STORAGE = 3.8
+const CELL_VOLTAGE_LOW = 3.5
+const CELL_VOLTAGE_DAMAGED = 3.3
+const DAMAGE_HEALTH_DROP_SPEED = 0.05
+
+export (int, 1, 6) var cells = 4
+export (int, 100, 10000) var capacity = 1300
+export (int, 1, 200) var c_rating = 100
+export (bool) var infinite = false
+
+var voltage = 0.0
+var current = 0.0
+var mah = 0.0
+var health = 1.0
+
+
+func _ready():
+	voltage = cells * CELL_VOLTAGE_FULL
+
+
+func _physics_process(delta):
+	# update voltage, current, mah
+	if current > capacity * c_rating or voltage < cells * CELL_VOLTAGE_DAMAGED:
+		health -= delta * DAMAGE_HEALTH_DROP_SPEED
