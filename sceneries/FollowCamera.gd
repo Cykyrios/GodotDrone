0f74e76ed0cef6865da7a817b6002f7b281a6ff3
Cykyrios <cykyrios@gmail.com>
2020-05-19 01:07:54 +0200

    Update follow camera

--- sceneries/FollowCamera.gd
+++ sceneries/FollowCamera.gd
@@ -14,11 +14,20 @@ func _ready():
 		target_node = get_node(target)
 
 
-func _process(delta):
+func _physics_process(delta):
 	if target_node is Spatial:
 		var target_pos = target_node.global_transform.origin + target_offset
 		var pos_diff = target_pos - global_transform.origin
-		var offset = target_node.global_transform.xform(camera_offset)
+		var target_forward = -target_node.global_transform.basis.z
+		target_forward = Vector3(target_forward.x, 0, target_forward.z)
+		var offset = Vector3(camera_offset.x, 0, camera_offset.z)
+		var cross_prod = offset.normalized().cross(target_forward.normalized()).y
+		var angle = asin(cross_prod)
+		if camera_offset.dot(target_forward) >= 0:
+			angle = PI + angle
+		else:
+			angle = -angle
+		offset = target_pos + camera_offset.rotated(Vector3.UP, angle)
 		var new_pos = target_pos - pos_diff.normalized() * target_distance
 		new_pos = new_pos.linear_interpolate(offset, speed * delta)
 		look_at_from_position(new_pos, target_pos, Vector3.UP)
