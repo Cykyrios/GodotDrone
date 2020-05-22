8ea074bc84a598bf61cb50aa80c5626f850ac5c3
Cykyrios <cykyrios@gmail.com>
2020-05-22 11:09:49 +0200

    Update large ring gate collision shape
    Replaces concave collision mesh with multiple capsule shapes, this
    eliminates collision detection issues.
    
    The script is used to duplicate the single capsule shape contained
    inside the scene along the ring.

---
+++ Assets/Gate/GateCollision.gd
@@ -0,0 +1,14 @@
+extends StaticBody
+
+
+onready var shape = $CollisionShape1
+
+
+func _ready():
+	var num = 32
+	for i in range(num):
+		var s = shape.duplicate()
+		add_child(s)
+		s.translate_object_local(Vector3(-6.25, 0, 0))
+		s.rotate_y(2 * PI * i / num)
+		s.translate_object_local(Vector3(6.25, 0, 0))
