ea35bea10b110c6c32a3f54b88df0049b14ab090
Cykyrios <cykyrios@gmail.com>
2022-09-24 21:54:17 +0200

    Fix spline cone paths

--- tracks/PathedObject.gd
+++ tracks/PathedObject.gd
@@ -1,6 +1,6 @@
 @tool
-extends Path3D
 class_name PathedObject
+extends Path3D
 
 
 @export var mesh: Mesh = null :
@@ -11,43 +11,6 @@ class_name PathedObject
 		multimesh.mesh = mesh
 		update_multimesh()
 var multimesh: MultiMesh = null
-@export_multiline var spline_points := "" :
-	set(points):
-		var points_array := []
-		points = points.replace("\n", "")
-		var string_array: Array = points.split("]")
-		string_array.remove_at(string_array.size() - 1)
-		for string in string_array:
-			if string.count("[") != 1:
-				return
-			else:
-				string = string.replace("[", "")
-			var vector_string: Array = string.split(")")
-			vector_string.remove_at(vector_string.size() - 1)
-			var vector_array := []
-			for substring in vector_string:
-				if substring.count("(") != 1:
-					return
-				else:
-					substring = substring.replace("(", "")
-					substring = substring.replace(" ", "")
-					if substring.begins_with(","):
-						substring = substring.lstrip(",")
-					var v: Array = substring.split(",")
-					if v.size() == 3:
-						vector_array.append(Vector3(v[0].to_float(), v[1].to_float(), v[2].to_float()))
-					else:
-						return
-			while vector_array.size() < 3:
-				vector_array.append(Vector3(0.0, 0.0, 0.0))
-			points_array.append(vector_array)
-		spline_points = ""
-		curve.clear_points()
-		for point in points_array:
-			spline_points += "[%s]\n" % [point]
-			curve.add_point(point[0], point[1], point[2])
-		update_multimesh()
-
 @export_range (0, 10) var spacing := 1.0 :
 	set(space):
 		spacing = space
@@ -56,12 +19,8 @@ var multimesh: MultiMesh = null
 
 func _ready() -> void:
 	var _discard = curve_changed.connect(_on_curve_changed)
-
-	update_spline_points()
-
 	if !multimesh:
 		create_multimesh()
-
 	update_multimesh()
 
 
@@ -73,16 +32,7 @@ func get_multimesh() -> MultiMesh:
 	return multimesh
 
 
-func update_spline_points() -> void:
-	spline_points = ""
-	for i in range(curve.get_point_count()):
-		spline_points += "[%s, %s, %s]\n" % [curve.get_point_position(i),
-				curve.get_point_in(i), curve.get_point_out(i)]
-	notify_property_list_changed()
-
-
 func _on_curve_changed() -> void:
-	update_spline_points()
 	update_multimesh()
 
 
